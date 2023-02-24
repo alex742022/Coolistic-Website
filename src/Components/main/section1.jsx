3f0d8c389522e2bb6c8698817475525913d0bcca
alex742022 <alexis.aquino.lumbera7@gmail.com>
2023-02-24 15:03:41 +0800

    added some elements

--- src/Components/main/section1.jsx
+++ src/Components/main/section1.jsx
@@ -17,48 +17,55 @@ export default function Section1() {
 
   return (
     <>
-      <header className="w-[90%] mt-[10px] sm:mb-[10px] lg:w-4/5 lg:mt-[16px] h-[70px] mx-auto max-w-[1444px] flex items-center justify-between">
-        <img className="w-[140px] sm:w-[175px]" src={Logo} alt="logo" />
+      <header className="w-[100%] sm:mb-[10px] lg:mt-[16px] py-[8px] fixed lg:static lg:py-[0px] z-[40] bg-white flex shadow-md lg:shadow-none">
+        <div className="w-[90%] lg:w-[80%] mx-auto max-w-[1444px] flex items-center justify-between ">
+          <img className="w-[140px] sm:w-[175px]" src={Logo} alt="logo" />
 
-        <div className="hidden lg:flex gap-x-[20px]">
-          {/* header location */}
-          <div className="flex items-center gap-[12px]">
-            <div className="w-[30px]">
-              <img className="" src={Location} alt="location" />
+          <div className="hidden lg:flex  gap-x-[20px]">
+            {/* header location */}
+            <div className="flex items-center gap-[12px]">
+              <div className="w-[30px]">
+                <img className="" src={Location} alt="location" />
+              </div>
+              <div className="leading-4">
+                <p className="font-bold btc2">171 Molave</p>
+                <p className="text-[#787878]">Parañaque, Metro Manila</p>
+              </div>
             </div>
-            <div className="leading-4">
-              <p className="font-bold btc2">171 Molave</p>
-              <p className="text-[#787878]">Parañaque, Metro Manila</p>
+            {/* header contact */}
+            <div className="flex items-center gap-[15px]">
+              <div className="w-[30px]">
+                <img className="" src={Contact} alt="location" />
+              </div>
+              <div className="leading-4">
+                <p className="font-bold btc2">0917 581 2765</p>
+                <p className="text-[#787878]">dan.coolistic@gmail.com</p>
+              </div>
             </div>
           </div>
-          {/* header contact */}
-          <div className="flex items-center gap-[15px]">
-            <div className="w-[30px]">
-              <img className="" src={Contact} alt="location" />
-            </div>
-            <div className="leading-4">
-              <p className="font-bold btc2">0917 581 2765</p>
-              <p className="text-[#787878]">dan.coolistic@gmail.com</p>
-            </div>
-          </div>
-        </div>
 
-        {/* menu icon */}
-        <div className="w-[30px] lg:hidden" onClick={toogleForNav}>
-          <img src={toogle ? Menu : Cancel} alt="menu-icon" />
+          {/* menu icon */}
+          <div className="w-[30px] lg:hidden" onClick={toogleForNav}>
+            <img src={toogle ? Menu : Cancel} alt="menu-icon" />
+          </div>
         </div>
       </header>
       {/* header 2 */}
       {/* onclick function for toogle */}
       <header
-        className={`absolute z-[20] w-[270px] ${
+        className={`fixed z-[20] w-[270px] ${
           toogle ? "right-[100%]" : "right-0"
-        } mt-[8px] py-[20px] bc2 flex lg:w-full lg:static lg:items-center lg:h-[72px]`}
+        } top-[90px] py-[20px] bc2 flex z-50 lg:w-full lg:sticky lg:items-center lg:top-0 lg:h-[72px] lg:shadow-sm`}
       >
         <div className="w-4/5 max-w-[1444px] mx-auto flex flex-col gap-[40px] lg:flex-row">
           <ul className="flex flex-col gap-[20px] lg:flex-row lg:gap-[40px] text-white text-[17px]">
-            <li>Home</li>
-            <li>About Us</li>
+            <a href="#Home">
+              <li>Home</li>
+            </a>
+            <a href="#About">
+              <li>About Us</li>
+            </a>
+
             <li>Our Services</li>
             <li>Pricing</li>
             <li>Contacts</li>
@@ -92,7 +99,10 @@ export default function Section1() {
           </div>
         </div>
       </header>
-      <main className="relative mx-auto h-[700px] overflow-y-hidden">
+      <main
+        className="relative pt-[70px] mx-auto h-[700px] overflow-y-hidden lg:pt-[0px]"
+        id="Home"
+      >
         <div className="flex w-[85%] max-w-[1440px] mx-auto lg:w-4/5 lg:h-[760px]">
           <div className="lg:w-[493px] mt-[160px] z-10">
             <h1 className="text-[40px] leading-[40px] md:text-[50px] md:leading-[50px] lg:text-[60px] font-[700] btc1 lg:leading-[60px]">
@@ -112,17 +122,21 @@ export default function Section1() {
             </a>
           </div>
           <div className="lg:absolute w-[50%] h-[700px] right-0">
-            <img className="w-[100%] object-cover object-top h-[700px]" src={CoolisticImg} alt="coolistic" />
+            <img
+              className="w-[100%] object-cover object-top h-[700px]"
+              src={CoolisticImg}
+              alt="coolistic"
+            />
           </div>
           <div className="absolute w-[100%] h-[700px] left-0 lg:hidden">
-              <img
-                className="w-[100%] object-cover sm:object-top h-[700px]"
-                src={CoolisticImgPhone}
-                alt="coolistic"
-              />
+            <img
+              className="w-[100%] object-cover sm:object-top h-[700px]"
+              src={CoolisticImgPhone}
+              alt="coolistic"
+            />
           </div>
         </div>
-        <div className="absolute bg-black w-[100%] h-[100%] top-0 opacity-[50%] lg:hidden"></div>  
+        <div className="absolute bg-black w-[100%] h-[100%] top-0 opacity-[40%] lg:hidden"></div>
       </main>
     </>
   );
