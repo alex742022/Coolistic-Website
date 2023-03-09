93095d78031efd5ae1044ca58d48d54aa4f45e44
alex742022 <alexis.aquino.lumbera7@gmail.com>
2023-03-09 11:46:42 +0800

    added new components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Footer from "./Components/main/footer";
 import Section2 from "./Components/main/section2";
 import Services from "./Components/main/Services/services";
 import BrandsAircon from "./Components/main/brandsAircon";
+import Certificates from "./Components/certificates/certificates";
 function App() {
   return (
     <div className="App">
       <Section1 />
       <Section2 />
       <Services />
+      <Certificates/>
       <BrandsAircon/>
       <Footer />
     </div>
